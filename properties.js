912be13932491f40b53862e00274669549ca71df
Manuubett <126500294+Manuubett@users.noreply.github.com>
2026-09-11 16:22:41 +0300

    Update properties.js

--- properties.js
+++ properties.js
@@ -24,7 +24,7 @@ function loadResults() {
   const type = params.get("type") || "";
   const maxPrice = params.get("maxPrice") ? Number(params.get("maxPrice")) : null;
 
-  db.collection("propertiesss")
+  db.collection("propertiess")
     .where("status", "==", "approved")
     .orderBy("createdAt", "desc")
     .get()
