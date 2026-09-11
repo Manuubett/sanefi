0e1ed0d40604bb1ad637b8fa834999fa274f0327
Manuubett <126500294+Manuubett@users.noreply.github.com>
2026-09-11 10:01:37 +0300

    Fix typo in Firestore collection name

--- properties.js
+++ properties.js
@@ -25,6 +25,7 @@ function loadResults() {
   const maxPrice = params.get("maxPrice") ? Number(params.get("maxPrice")) : null;
 
   db.collection("propertiess")
+    .where("status", "==", "approved")
     .orderBy("createdAt", "desc")
     .get()
     .then((snapshot) => {
